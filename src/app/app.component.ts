6be7fb718a183933151760502ca5c4bec67cb80e
Eugene Pidtserkovnyi <pidtsierkovnii@gmail.com>
2023-11-06 15:15:02 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title='TestAngular';
+  password = '';
+  strength = 'E';
+ 
+  updateStrength() {
+     if (!this.password) {
+       this.strength = 'E';
+     } else if (this.password.length < 8) {
+       this.strength = 'NE';
+     } else {
+       let hasLetter = /[a-zA-Z]/.test(this.password);
+       let hasDigit = /\d/.test(this.password);
+       let hasSymbol = /[^a-zA-Z\d]/.test(this.password);
+ 
+       if (hasLetter && hasDigit && hasSymbol) {
+         this.strength = 'S';
+       } else if (hasLetter && hasDigit || hasLetter && hasSymbol || hasDigit && hasSymbol) {
+         this.strength = 'M';
+       } else if(hasLetter || hasDigit || hasSymbol) {
+         this.strength = 'L';
+       }
+     }
+  }
+ }
